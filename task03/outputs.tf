abaef0fccafd959ab588c3db6363dddcf8d1e227
Mark <mark_nagy@epam.com>
2025-08-29 14:14:12 +0000

    asd

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,14 +1,14 @@
 output "rg_id" {
-  value = azurerm_resource_group.rg.id
+  value       = azurerm_resource_group.rg.id
   description = "The ID of the resource group"
 }
 
 output "sa_blob_endpoint" {
-  value = azurerm_storage_account.sa.primary_blob_endpoint
+  value       = azurerm_storage_account.sa.primary_blob_endpoint
   description = "Primary blob endpoint of the storage account"
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  value       = azurerm_virtual_network.vnet.id
   description = "The ID of the virtual network"
 }
