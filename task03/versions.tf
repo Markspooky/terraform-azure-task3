852634e6722c5a830642e01eaf091a49c11b83de
Mark <mark_nagy@epam.com>
2025-08-29 14:05:00 +0000

    files

--- task03/versions.tf
+++ task03/versions.tf
@@ -1,17 +1,11 @@
 terraform {
   required_version = ">= 1.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "cmaz-zz733vvq-mod3-rg"
-    storage_account_name = "cmazzz733vvqsa"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
   }
 }
 
